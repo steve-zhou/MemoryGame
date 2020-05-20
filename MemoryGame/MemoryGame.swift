0d91f9cfea66417922dad07acd35eff6b8bc26b8
Steve Zhou <steve.zhou79@gmail.com>
2020-05-20 14:32:06 -0400

    Add Memory Game

---
+++ MemoryGame/MemoryGame.swift
@@ -0,0 +1,34 @@
+//
+//  MemoryGame.swift
+//  MemoryGame
+//
+//  Created by Steve Zhou on 5/20/20.
+//  Copyright © 2020 Steve Zhou. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    
+    var cards: [Card]
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = [Card]()
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(id: pairIndex * 2, content: content))
+            cards.append(Card(id: pairIndex * 2 + 1, content: content))
+        }
+    }
+    
+    func choose(card: Card){
+        print("Card Chosen: \(card)")
+    }
+    
+    struct Card: Identifiable {
+        var id: Int
+        var isFaceUP: Bool = true
+        var isMatched: Bool = false
+        var content: CardContent
+    }
+}
