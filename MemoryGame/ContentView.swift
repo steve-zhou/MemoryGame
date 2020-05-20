3213c0c9695a47dfe805f597bde651f21721944a
Steve Zhou <steve.zhou79@gmail.com>
2020-05-20 14:17:29 -0400

    Project Created

---
+++ MemoryGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemoryGame
+//
+//  Created by Steve Zhou on 5/20/20.
+//  Copyright © 2020 Steve Zhou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
